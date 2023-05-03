9085c8726005c72040ffb9c5e7c5b2037c902dbf
AlbertoDPH <114424982+AlbertoDPH@users.noreply.github.com>
2023-05-04 01:14:18 +0200

    UD30/Ejercicio3

---
+++ Ejercicio3/js/meses.js
@@ -0,0 +1,20 @@
+//  array  con los meses del año
+const meses = [
+    "Enero",
+    "Febrero",
+    "Marzo",
+    "Abril",
+    "Mayo",
+    "Junio",
+    "Julio",
+    "Agosto",
+    "Septiembre",
+    "Octubre",
+    "Noviembre",
+    "Diciembre"
+];
+
+// Muestra por pantalla los meses utilizando console.log()
+meses.forEach(function (mes) {
+    console.log(mes);
+});
